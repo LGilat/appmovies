af6bc611abe5a0c8d9644b4dadce046e04c3161a
Lgilat <luigilbla@alu.edu.gva.es>
2024-09-24 19:45:33 +0200

    'commit build_files'

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Activa el entorno virtual
+source pyvenv/bin/activate
+
+
 python3 -m pip install -r requirements.txt
+# Realiza las migraciones
+python manage.py migrate
+
+
 python3 manage.py collectstatic --noinput
+
