d372e9b76d7c684dbd982eb2511b782448649b3e
Lgilat <luigilbla@alu.edu.gva.es>
2024-09-24 20:00:55 +0200

    'cambios en build_files,requirements'

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Activa el entorno virtual
-source pyvenv/bin/activate
-
+python3 -m pip install --upgrade pip
 
 python3 -m pip install -r requirements.txt
-# Realiza las migraciones
-python manage.py migrate
+
 
 
 python3 manage.py collectstatic --noinput
