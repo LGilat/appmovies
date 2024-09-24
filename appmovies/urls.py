aee188005b2130d411b53469305d16690fe42de8
Lgilat <luigilbla@alu.edu.gva.es>
2024-09-24 22:35:01 +0200

    'modified url.py and vercel'

--- appmovies/urls.py
+++ appmovies/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('movies/', include('movies.urls'), name='movies'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
