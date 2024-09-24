93d01a9b3ae5d8606e73ea11f13d40066fe1c038
Lgilat <luigilbla@alu.edu.gva.es>
2024-09-24 22:59:23 +0200

    'retro urls.py'

--- appmovies/urls.py
+++ appmovies/urls.py
@@ -26,5 +26,3 @@ urlpatterns = [
     path('movies/', include('movies.urls'), name='movies'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
